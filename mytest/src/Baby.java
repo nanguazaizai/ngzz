f133288514b89263d3811305f75083acc469e227
ngzz <1403919853@qq.com>
2021-10-04 15:47:41 +0800

    在branch01中给Baby类加了sleep方法 mytest

--- mytest/src/Baby.java
+++ mytest/src/Baby.java
@@ -1,6 +1,9 @@
-public class Baby extends People{
+public class Baby implements People {
     @Override
     public void eat() {
         System.out.println("喝奶奶");
+    }
+    public void sleep(){
+        System.out.println("睡觉觉");
     }
 }
